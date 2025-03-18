28f608292e4682c4bc1e414cf0191d77d2da44d2
TCO46 <patohru2006@gmail.com>
2025-03-18 22:56:45 +0700

    Add update and delete product

--- product.c
+++ product.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include "product.h"
-#include "cart.h"
 
 void clearInputBuffer() {
     int c;
@@ -21,11 +20,211 @@ void addProduct(Product *products, int *count, const char *name, float price, in
     (*count)++;
 }
 
+void updateProduct(Product *product, int count) {
+    FILE *file = fopen(PRODUCT_FILE, "r");
+    FILE *temp = fopen(PRODUCT_TEMP_FILE, "w");
+
+    if(!file) {
+        printf("Error opening file!\n");
+        return;
+    }
+
+    char line[512];
+
+    int id = 0;
+
+    printf("ID | Name                           | Price       | Stock | Category\n");
+    printf("-------------------------------------------------------------------------\n");
+    
+    while (fgets(line, sizeof(line), file)) {
+        char name[100], description[255], category[50];
+        double price;
+        int quantity;
+
+        id++;
+
+        sscanf(line, "%99[^,],%lf,%d,%255[^,],%49[^\n]", name, &price, &quantity, description, category);
+
+        printf("%-2d | %-30s | $%-10.2lf | %-5d | %-6s\n", id, name, price, quantity, category);
+    }
+
+    int updateId;
+
+    printf("\nPress \"-1\" to exit.");
+    printf("\nEnter product number you want to update: ");
+    scanf("%d", &updateId);
+
+    if (updateId > id || updateId < -1) {
+        printf("%d is too large or too small!!", updateId);
+        return;
+    }
+
+    if(updateId == -1) {
+        printf("\nPress any key to return...");
+
+        return;
+    }
+    id = 0;
+    char productName[100];
+    int choice;
+
+    rewind(file);
+
+    while (fgets(line, sizeof(line), file)) 
+    {
+        char name[100], description[255], category[50];
+        float price;
+        int quantity;
+
+        sscanf(line, "%99[^,],%f,%d,%255[^,],%49[^\n]", name, &price, &quantity, description, category);
+
+        id++;
+
+        if (id != updateId) { 
+            fprintf(temp, "%s,%.2f,%d,%s,%s\n", name, price, quantity, description, category);
+        }
+
+        if(id == updateId) {
+            printf("Current detail: \n");
+
+            printf("ID | Name                           | Price       | Stock | Category\n");
+            printf("-------------------------------------------------------------------------\n");
+            printf("%-2d | %-30s | $%-10.2lf | %-5d | %-6s\n", id, name, price, quantity, category);
+
+            printf("\nWhat do you want to update?\n");
+            printf("1. Name\n");
+            printf("2. Price\n");
+            printf("3. Category\n");
+            printf("Enter your choice: ");
+            scanf("%d", &choice);
+
+            clearInputBuffer();
+
+            if (choice == 1) {
+                printf("Enter new name: ");
+                scanf("%[^\n]s", name);
+            } else if (choice == 2) {
+                printf("Enter new price: ");
+                scanf("%f", &price);
+            } else if (choice == 3) {
+                printf("Enter new category: ");
+                scanf("%s", category);
+            } else {
+                printf("Invalid choice!\n");
+                return;
+            }
+
+            fprintf(temp, "%s,%.2f,%d,%s,%s\n", name, price, quantity, description, category);
+
+            break;
+        }
+        else {
+            strcpy(productName, name);
+        }
+    }
+
+    fclose(file);
+    fclose(temp);
+
+    remove(PRODUCT_FILE);
+    rename(PRODUCT_TEMP_FILE, PRODUCT_FILE);
+
+    printf("Updated \"%s\" from DB (txt file)\n", productName);
+    printf("\nPress any key to return...");
+}
+
+void deleteProduct(Product *product, int count) {
+    FILE *file = fopen(PRODUCT_FILE, "r");
+    FILE *temp = fopen(PRODUCT_TEMP_FILE, "w");
+
+    if (!file) {
+        printf("error opening file!\n");
+        return;
+    }
+
+    char line[512];
+
+    int id = 0;
+
+    printf("ID | Name                           | Price       | Stock | Category\n");
+    printf("-------------------------------------------------------------------------\n");
+
+    while (fgets(line, sizeof(line), file)) {
+        char name[100], description[255], category[50];
+        float price;
+        int quantity;
+
+        id++;
+
+        sscanf(line, "%99[^,],%f,%d,%255[^,],%49[^\n]", name, &price, &quantity, description, category);
+
+        printf("%-2d | %-30s | $%-10.2f | %-5d | %-6s\n", id, name, price, quantity, category);
+    }
+
+    if (id == 0) {
+        printf("There is no products left (we gone bankrupted)");
+
+        fclose(file);
+        fclose(temp);
+
+        return;
+    }
+
+    int removeId;
+
+    printf("\nPress \"-1\" to exit.");
+    printf("\nEnter product number you want to remove: ");
+    scanf("%d", &removeId);
+
+    if (removeId > id || removeId < -1) {
+        printf("%d is too large or too small!!", removeId);
+        return;
+    }
+
+    if(removeId == -1) {
+        printf("\nPress any key to return...");
+
+        return;
+    }
+
+    id = 0;
+    char productName[100];
+
+    rewind(file);
+
+    while (fgets(line, sizeof(line), file)) 
+    {
+        char name[100], description[255], category[50];
+        float price;
+        int quantity;
+
+        sscanf(line, "%99[^,],%f,%d,%255[^,],%49[^\n]", name, &price, &quantity, description, category);
+
+        id++;
+
+        if (id != removeId) { 
+            fprintf(temp, "%s,%.2f,%d,%s,%s\n", name, price, quantity, description, category);
+        }
+        else {
+            strcpy(productName, name);
+        }
+    }
+
+    fclose(file);
+    fclose(temp);
+
+    remove(PRODUCT_FILE);
+    rename(PRODUCT_TEMP_FILE, PRODUCT_FILE);
+
+    printf("Removed \"%s\" from DB (txt file)\n", productName);
+    printf("\nPress any key to return...");
+}
+
 void addProductToDB() {
     Product product;
     FILE *file = fopen(PRODUCT_FILE, "a");
     if (!file) {
-        printf("Error opening file!\n");
+        printf("error opening file!\n");
         return;
     }
 
@@ -69,26 +268,27 @@ void searchProduct(Product *products, int count) {
     char keyword[100];
     printf("Enter the keyword or category: ");
 
-    getchar();  // Đọc ký tự '\n' còn sót lại từ scanf trước đó
+    getchar();
 
-    fgets(keyword, sizeof(keyword), stdin);  // Dùng fgets để đọc chuỗi có khoảng trắng
+    fgets(keyword, sizeof(keyword), stdin);
     keyword[strcspn(keyword, "\n")] = 0;
 
 
     int found = 0;
-    printf("ID   | Name                           | Price       | Stock | Category\n");
+    printf("ID  | Name                           | Price       | Stock | Category\n");
     printf("-------------------------------------------------------------------------\n");
 
     for (int i = 0; i < count; i++) {
         if (strstr(products[i].name, keyword) != NULL || strstr(products[i].category, keyword) != NULL) {
-            printf("%-2d | %-30s | $%-10.2f | %-5d | %-6s\n", products[i].count, products[i].name, products[i].price, products[i].quantity,  products[i].category);
-            printf("\n");
+            printf("%-2d  | %-30s | $%-10.2f | %-5d | %-6s\n", products[i].count, products[i].name, products[i].price, products[i].quantity,  products[i].category);
             found = 1;
         }
     }
     if (!found) {
         printf("No products found matching the keyword: %s\n", keyword); 
     }
+    
+    printf("\n\nPress any key to return...\n");
 }
 
 // In tất cả sản phẩm
@@ -100,12 +300,11 @@ void displayProducts(Product *products, int count) {
     printf("ID  | Name                           | Price       | Stock | Category\n");
     printf("-------------------------------------------------------------------------\n");
 
-    for (int i = 0; i < count; i++) {
+    for (int i = 1; i < count; i++) {
         printf("%-2d  | %-30s | $%-10.2f | %-5d | %-6s\n", products[i].count, products[i].name, products[i].price, products[i].quantity,  products[i].category);
-        printf("\n");
     }
 
-    printf("Press Enter to continue.\n");
+    printf("\nPress Enter to continue...\n");
 }
 
 // Đọc sản phẩm từ file PRODUCT_FILE
--- product.h
+++ product.h
@@ -7,18 +7,21 @@
 #define MAX_DESCRIPTION_CHAR 255
 #define MAX_CATEGORY_CHAR 50
 
-#define PRODUCT_FILE "product.txt"  // Định nghĩa tên file sản phẩm
+#define PRODUCT_FILE "product.txt"
+#define PRODUCT_TEMP_FILE "tempProduct.txt"
 
 typedef struct {
     int count;
     char name[100];
     float price;
     int quantity;
-    char description[255];
+    char description[256];
     char category[50];
 } Product;
 
 void addProduct(Product *products, int *count, const char *name, float price, int quantity, const char *description, const char *category);
+void updateProduct(Product *product, int count);
+void deleteProduct(Product *product, int count);
 void searchProduct(Product *products, int count);
 void displayProducts(Product *products, int count);
 void addProductToDB();
